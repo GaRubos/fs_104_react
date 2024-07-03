f47b9c61d3cdd5f44a487995751908f95508f3dd
Ruslan Garmash <garmash.rb@gmal.com>
2024-07-03 21:01:22 +0300

    counter & rules done

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,25 +1,33 @@
+import { useState } from "react";
+import s from "./Counter.module.css";
+
 const Counter = () => {
-  const handleClick = () => {
-    console.log(Date.now());
+  const [counter, setCounter] = useState(0);
+
+  const handlePlusClick = () => {
+    setCounter((prev) => prev + 1);
   };
 
-  const handleMinusClick = (name) => {
-    console.log(`${Date.now()} - Ruslan`);
+  const handleMinusClick = () => {
+    if (counter <= -5) {
+      return alert("Oooops");
+    }
+
+    setCounter((prev) => prev - 1);
   };
 
-  const resetClick = (e) => {
-    console.log(e);
-    e.target.innerHTML = "hello";
+  const resetClick = () => {
+    setCounter(0);
   };
 
   return (
-    <div>
-      <div>
-        <h1>{1}</h1>
+    <div className={s.counter}>
+      <div className={s.wrapper}>
+        <h1>{counter}</h1>
         <div>
-          <button onClick={() => handleMinusClick("ruslan")}>minus</button>
+          <button onClick={handleMinusClick}>minus</button>
           <button onClick={resetClick}>reset</button>
-          <button onClick={handleClick}>plus</button>
+          <button onClick={handlePlusClick}>plus</button>
         </div>
       </div>
     </div>
